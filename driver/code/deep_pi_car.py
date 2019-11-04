cb7e41da48fb5a4983ea21c92a66fda91f8e558f
jiminngkang <jimingkang@gmail.com>
2019-11-04 17:20:15 -0500

    delete tunr.ahead() in drive()  in deep_pi_car

--- driver/code/deep_pi_car.py
+++ driver/code/deep_pi_car.py
@@ -129,9 +129,9 @@ class DeepPiCar(object):
         logging.info('Starting to drive at speed %s...' % speed)
         #self.back_wheels.speed = speed
         i = 0
-        turn.ahead()
+       
         motor.motor_left(status, backward,left_spd)
-        motor.motor_right(status,forward,right_spd)
+       
         while self.camera.isOpened():
             _, image_lane = self.camera.read()
             image_objs = image_lane.copy()
