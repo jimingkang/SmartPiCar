80d7c070706ea1b2a87c76aaf985c21cf43954a1
jiminngkang <jimingkang@gmail.com>
2019-11-06 11:32:04 -0500

    change to turn.py instead of frontwheel

--- driver/code/hand_coded_lane_follower.py
+++ driver/code/hand_coded_lane_follower.py
@@ -86,8 +86,8 @@ class HandCodedLaneFollower(object):
         if self.car is not None:
             logging.error('curr_steering_angle:')
             logging.error(self.curr_steering_angle)
-            turn.turn_ang(self.curr_steering_angle-90)
-            #self.car.front_wheels.turn(self.curr_steering_angle)
+            #turn.turn_ang(self.curr_steering_angle-90)
+            self.car.front_wheels.turn(self.curr_steering_angle-90)
             motor.motor_left(status, forward,80)
             motor.motor_right(status, backward,80)
             
