419fd8e819ce78b5af88a0b871b083140e53e191
jiminngkang <jimingkang@gmail.com>
2019-11-06 07:51:14 -0500

    test motor_right

--- driver/code/motor.py
+++ driver/code/motor.py
@@ -96,7 +96,7 @@ except KeyboardInterrupt:
 
 if __name__ == '__main__':
     setup()
-    while(True)
+    while(True):
         motor_left(status, forward,30)
         motor_right(status,backward,30)
 
