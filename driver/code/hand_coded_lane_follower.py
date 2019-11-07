42fca983227cca10ce55a23a7fa7702963bcaf5d
jiminngkang <jimingkang@gmail.com>
2019-11-07 12:38:53 -0500

    test turn in handed_code

--- driver/code/hand_coded_lane_follower.py
+++ driver/code/hand_coded_lane_follower.py
@@ -94,14 +94,18 @@ class HandCodedLaneFollower(object):
            
             
             dis_front = ultra.checkdist()
-            if dis_front < distance_front:
-                self.car.front_wheels.turn(135)
-                motor.motor_left(status, backward,50)
-                motor.motor_right(status,forward,50)
+            while(dis_front < distance_front):
+                if self.curr_steering_angle<90:
+                    self.car.front_wheels.turn_left()
+                else:
+                    self.car.front_wheels.turn_right()
+                
+                motor.motor_left(status, backward,80)
+                motor.motor_right(status,forward,80)
                 #self.car.front_wheels.turn(90)
             else:
                 time.sleep(1)
-                self.car.front_wheels.turn(self.curr_steering_angle)
+                self.car.front_wheels.turn_ang(self.curr_steering_angle)
                 motor.motor_left(status, forward,50)
                 motor.motor_right(status, backward,50)
 
