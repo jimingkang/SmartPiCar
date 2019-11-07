6305be26abc7ce2592792d126a0c7682686bc72c
jiminngkang <jimingkang@gmail.com>
2019-11-07 12:09:19 -0500

    test turn middle _angle=370

--- driver/code/deep_pi_car.py
+++ driver/code/deep_pi_car.py
@@ -147,7 +147,7 @@ class DeepPiCar(object):
                 motor.motor_right(status,forward,right_spd)
                 self.front_wheels.turn(90)
             else:
-                sleep(1)
+                time.sleep(1000)
                 motor.motor_left(status, forward,50)
                 motor.motor_right(status, backward,50)
             
